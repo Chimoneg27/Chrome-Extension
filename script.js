09f1f82c5426ef7d724bc08cea2b1861be1d183d
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2024-06-06 10:15:18 +0200

    Add event listener

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
-function saveInput() {
-    console.log("Button Clicked!")
-}
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+
+inputBtn.addEventListener("click", function() {
+    console.log("Button clicked!")
+})
