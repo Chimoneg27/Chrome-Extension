a5669b00d432cc7cb183a46c16e63b7831878c22
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2024-06-06 10:53:11 +0200

    Add ul element

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
+let ulEl = document.getElementById('ul-el')
 
 inputBtn.addEventListener("click", function() {
-    console.log("Button clicked!")
+    myLeads.push(inputEl.value)
+    console.log(myLeads)
 })
+
+for (let i = 0; i < myLeads.length; i++) {
+    ulEl.textContent += myLeads[i] + " "
+}
