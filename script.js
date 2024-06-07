cb79682b5e908c53674c18510dfa84dacb712fa6
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2024-06-07 21:47:37 +0200

    Add innerHTML

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-let myLeads = []
+let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
-let ulEl = document.getElementById('ul-el')
+const ulEl = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     console.log(myLeads)
 })
 
+// Replace .textContent with .innerHTML and use <li> tags
 for (let i = 0; i < myLeads.length; i++) {
-    ulEl.textContent += myLeads[i] + " "
+    ulEl.innerHTML += `<li>${myLeads[i]}</li>`
 }
